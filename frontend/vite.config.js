8ea918f3d3d09eddc652e366477c2e4bc944be60
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-07-15 08:11:52 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills' // <-- 1. Import the plugin
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills(), // <-- 2. Add the plugin here
-  ],
-})
+  plugins: [react()],
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      // This is the polyfill for the 'buffer' module
+      buffer: 'buffer/',
+    },
+  },
+});
